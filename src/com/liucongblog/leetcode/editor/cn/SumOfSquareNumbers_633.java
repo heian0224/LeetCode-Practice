1f90c8799c5dbee85e4167769df93ec9313a9c44
cong.liu <cong.liu@saxofintech.com>
2024-11-04 10:28:05 +0800

    feat: add solution for LeetCode problem 633
    - Implement the solution for checking if a number can be expressed as the sum of two squares
    - Add documentation for the problem in markdown format
    
    This feature introduces a new LeetCode problem solution and includes the necessary documentation to explain the problem and its constraints.

---
+++ src/com/liucongblog/leetcode/editor/cn/SumOfSquareNumbers_633.java
@@ -0,0 +1,45 @@
+/**
+<p>Given a non-negative integer <code>c</code>, decide whether there're two integers <code>a</code> and <code>b</code> such that <code>a<sup>2</sup> + b<sup>2</sup> = c</code>.</p>
+
+<p>&nbsp;</p> 
+<p><strong class="example">Example 1:</strong></p>
+
+<pre>
+<strong>Input:</strong> c = 5
+<strong>Output:</strong> true
+<strong>Explanation:</strong> 1 * 1 + 2 * 2 = 5
+</pre>
+
+<p><strong class="example">Example 2:</strong></p>
+
+<pre>
+<strong>Input:</strong> c = 3
+<strong>Output:</strong> false
+</pre>
+
+<p>&nbsp;</p> 
+<p><strong>Constraints:</strong></p>
+
+<ul> 
+ <li><code>0 &lt;= c &lt;= 2<sup>31</sup> - 1</code></li> 
+</ul>
+
+<div><div>Related Topics</div><div><li>数学</li><li>双指针</li><li>二分查找</li></div></div><br><div><li>👍 475</li><li>👎 0</li></div>
+*/
+ 
+package com.liucongblog.leetcode.editor.cn;
+public class SumOfSquareNumbers_633{
+    public static void main(String[] args) {
+         Solution solution = new SumOfSquareNumbers_633().new Solution();
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+}
