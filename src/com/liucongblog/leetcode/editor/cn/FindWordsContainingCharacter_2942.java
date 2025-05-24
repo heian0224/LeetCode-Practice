c213e98392c46f4487b166eadd9afa8aaae22926
Liu Cong <heian0224@gmail.com>
2025-05-24 23:36:26 +0800

    add problem 2942

--- src/com/liucongblog/leetcode/editor/cn/FindWordsContainingCharacter_2942.java
+++ src/com/liucongblog/leetcode/editor/cn/FindWordsContainingCharacter_2942.java
@@ -58,7 +58,7 @@ class Solution {
     public List<Integer> findWordsContaining(String[] words, char x) {
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < words.length; i++) {
-            if (words[i].indexOf((int)x)>=0) {
+            if (words[i].indexOf( x)>=0) {
                 list.add(i);
             }
         }
